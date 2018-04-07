5c421660a7c82df38c98adc013c4e32c4ea95863
Nikos Mavrogeorgis <mavrogeorgis.nikos@gmail.com>
2018-04-07 15:32:07 +0300

    First working attempt for densem

--- denotational_semantics/src/densem.hs
+++ denotational_semantics/src/densem.hs
@@ -31,67 +31,59 @@ type S = Var -> Integer
 
 -- Commands
 
-semC :: C -> S -> S
-semC Cskip s = s
-semC (Cseq c1 c2) s = semC c2 (semC c1 s)
+semC :: C -> S -> (S, Integer)
+semC Cskip s = (s, -1)
+semC (Cseq c1 c2) s = semC c2 (fst (semC c1 s))
 semC (Cif b c1 c2) s | semB b s  = semC c1 s
                      | otherwise = semC c2 s
-semC (Cfor n c) s = expon i (semC c) s
-  where i = semN n s
+semC (Cfor n c) s = expon i (semC c) (s, 0)
+  where i = snd (semN n s)
 semC (Cwhile b c) s = fix bigF s
-  where bigF f s | semB b s  = f (semC c s)
-                 | otherwise = s
+  where bigF f s | semB b s  = f (fst (semC c s))
+                 | otherwise = (s, 0)
 
 -- Numbers
 
-semN :: N -> S -> Integer
-semN Nzero s = 0
-semN (Nsucc n) s = semN n s + 1
-semN (Npred n) s = semN n s - 1
-semN (Nvar x) s = s x
-semN (Nassign x n) s = update s x (semN n s)
-semN (Npp x) = 
-semN (Nmm x) = 
+semN :: N -> S -> (S, Integer)
+semN Nzero s = (s,0)
+semN (Nsucc n) s = (s, value + 1)
+	where value = snd (semN n s)
+semN (Npred n) s = (s, value - 1)
+	where value = snd (semN n s)
+semN (Nvar x) s = (s, s x)
+semN (Nassign x n) s = (update s x value, value)
+	where value = snd (semN n s)
+semN (Npp x) s = (update s x n, n)
+	where n = snd (semN (Nvar x) s) + 1 
+semN (Nmm x) s = (update s x n, n)
+	where n = snd (semN (Nvar x) s) - 1 
 
 -- Booleans
 
 semB :: B -> S -> Bool
 semB Btrue s = True
 semB Bfalse s = False
-semB (Blt n1 n2) s = semN n1 s < semN n2 s
-semB (Beq n1 n2) s = semN n1 s == semN n2 s
+semB (Blt n1 n2) s = snd (semN n1 s) < snd (semN n2 s)
+semB (Beq n1 n2) s = snd (semN n1 s) == snd (semN n2 s)
 semB (Bnot b) s = not (semB b s)
 
 -- auxiliary functions
 
 expon 0 f = id
-expon n f = f . expon (n-1) f
+expon n f = f . fst . expon (n-1) f
 
 update s x n y | x == y    = n
                | otherwise = s y
 
 -- example
-
+ 
 makeN 0 = Nzero
 makeN n = Nsucc (makeN (n-1))
 
 s0 x = error ("not initialized variable " ++ x)
 
-run c = print (semC c s0 "result")
+run c = print (fst (semN c s0) "result")
 
-ex0 = Cassign "result" (makeN 42)
-
-ex1 = Cseq (Cassign "result" Nzero)
-           (Cfor (makeN 6) (
-              Cfor (makeN 7) (
-                Cassign "result" (Nsucc (Nvar "result"))
-              )
-           ))
-
-ex2 = Cseq (Cassign "x" (makeN 42))
-      (Cseq (Cassign "result" Nzero)
-            (Cwhile (Blt Nzero (Nvar "x"))
-              (Cseq (Cassign "x" (Npred (Nvar "x")))
-                    (Cassign "result" (Nsucc (Nvar "result"))))))
+ex0 = Nassign "result" (makeN 42)
 
 fix f = f (fix f)
