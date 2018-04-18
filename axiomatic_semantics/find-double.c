5c0cbb019e5f69eef1225766b766ef9097e712f4
Nikos Mavrogeorgis <mavrogeorgis.nikos@gmail.com>
2018-04-19 01:29:15 +0300

    Some working - Axiomatic Semantics

--- axiomatic_semantics/find-double.c
+++ axiomatic_semantics/find-double.c
@@ -2,15 +2,15 @@
 #define MAXV 1000000 
 
 /*@
-  @ predicate hasDouble{L} (integer N, int *f, integer i) = 
-  @		\exists integer j; 0 <= j <= N - 1 && i != j && \at(x[i], L) == \at(x[j], L);
+  @ predicate hasDouble{L} (integer N, int *a, integer i) = 
+  @		\exists integer j; 0 <= j <= N - 1 && i != j && \at(a[i], L) == \at(a[j], L);
   @*/
 
 /*@
-  @ predicate existsDoubleInRange{L} (integer N, int *x, integer i) = 
-  @		i >= N ? false :
-  @		hasDoubleInRange{L}(N, x, i) ? true   
-  @						      		 : hasDouble{L}(N, x, i + 1);
+  @ predicate existsDoubleInRange{L} (integer N, int *a, integer i) = 
+  @		i >= N ? \false :
+  @		hasDouble{L}(N, a, i) ? \true   
+  @				       		  : hasDouble{L}(N, a, i + 1);
   @*/ 
 
 /*
@@ -21,21 +21,30 @@
 
 /*@ 
   @ requires N >= 1 && N <= 1000000;
-  @ requires \valid (f + (0 .. N - 1));
-  @ ensures \forall integer i; 0 <= i <= N - 1 ==> f[i] == \old(f[i]);
-  @ ensures existsDoubleInRange(N, x, 0) ==> \result == 1;
-  @ ensures !(existsDoubleInRange(N, x, 0)) ==> \result == 0;
+  @ requires \valid (a + (0 .. N - 1));
+  @ requires \forall integer i; 0 <= i < N ==> a[i] >= 1 && a[i] <= 1000000;
+  @ ensures \forall integer i; 0 <= i < N ==> a[i] == \old(a[i]);
+  @ ensures existsDoubleInRange(N, a, 0) ? \result != 0 : \result == 0;
   @*/
 int findDouble(int N, int a[]) { 
 	
 	bool f[MAXV]; 
 	
-	for (int i = 1; i <= MAXV; ++i) f[i-1] = false; 
-	//@ assert \forall integer i; 0 <= i <= N - 1 ==> f[i] == false;
+	/*@
+	  @ loop invariant 1 <= i <= MAXV + 1;
+	  @ loop assigns i, f[0 .. (MAXV - 1)];
+	  @*/
+	for (int i = 1; i <= MAXV; ++i) {
+		f[i-1] = false; 
+		//@ assert f[i - 1] == \false;
+	}
 	
 	/*@
 	  @ loop invariant 0 <= i <= N;
-	  @ loop invariant \forall integer i; 0 <= i <= N ==> !(has
+	  @ loop invariant !(existsDoubleInRange(i, a, 0));
+	  @ loop assigns i, f[0 .. (MAXV - 1)];
+	  @ loop variant N - i;
+	  @*/
 	for (int i = 0; i < N; ++i) 
 		if (f[a[i]-1]) return a[i]; 
 		else f[a[i]-1] = true; 
