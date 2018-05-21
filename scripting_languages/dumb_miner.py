ddbf5f758e4db38903fc8e5338230ef07ba32b08
Nikos Mavrogeorgis <mavrogeorgis.nikos@gmail.com>
2018-05-22 01:31:42 +0300

    Working. Fix play again left

--- scripting_languages/dumb_miner.py
+++ scripting_languages/dumb_miner.py
@@ -1,4 +1,5 @@
 import sys
+import re
 import requests
 import urllib
 import urllib2
@@ -17,7 +18,8 @@ def extract_magic(hex_str):
     output = hashlib.sha256(data).hexdigest()
     data = binascii.a2b_hex(output)
     output = hashlib.sha256(data).hexdigest()
-    return output[0 : 4]
+    return (output[0 : 4], output[5 : 9])
+
 
 def get_magic_and_debt(url, s):
 
@@ -37,24 +39,49 @@ def get_magic_and_debt(url, s):
     debt = float(debt)
     return (magic, debt)
 
+def get_post_response(url, s, values):
+
+    r = s.post(url, values)
+    html = r.content
+
+    lines = html.splitlines()
+    for line in lines:
+        line = line.lstrip()
+        tokens = line.split()
+        if (line.startswith("<span class=\"right\">")):
+            response = re.sub(r'<[^>]*>', '', line)
+            break
+    
+    return response
+
+
 s = requests.Session()
 url = sys.argv[1]
 
 data = get_magic_and_debt(url, s)
 magic = data[0]
 debt = data[1]
-print 'Magic is : ' + magic
-print 'Debt is : ', debt
 
-#while debt > 0 :
-for bitcoin in gen_all_hex():
-        bitcoin_magic = extract_magic(bitcoin)
-        if (bitcoin_magic == magic):
-            values = {'answer': bitcoin}
-            r = s.post(url, values)
-            fp = r.content
-            print fp
-            break
+counter = 1
+while debt > 0 :
+    print 'Round', counter, 'magic code: ', magic
+    for bitcoin in gen_all_hex():
+            bitcoin_data = extract_magic(bitcoin)
+            bitcoin_magic = bitcoin_data[0]
+            bitcoin_value = int(bitcoin_data[1], 16)
+            bitcoin_value = bitcoin_value / float(100)
+            if (bitcoin_magic == magic):
+                values = {'answer': bitcoin}
+                response = get_post_response(url, s, values)
+                #print fp
+                break
+    print bitcoin, bitcoin_value
+    print response
+    data = get_magic_and_debt(url, s)
+    magic = data[0]
+    debt = data[1]
+    counter += 1
+
 
 
 
